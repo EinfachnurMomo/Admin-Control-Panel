25b5f4c2360cac31c05477560932adc1f6f54ca3
kenny <kenny@DESKTOP-8771NQL>
2023-03-25 19:13:05 +0100

    Projektdateien hinzufügen.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Admin_Control_Panel.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
